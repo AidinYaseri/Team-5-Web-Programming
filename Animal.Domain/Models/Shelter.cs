a71b40a4797379d90e2a37d2af704cdbadfc551f
Darcy-Dilio <196205362+Darcy-Dilio@users.noreply.github.com>
2026-09-24 12:10:56 -0400

    Added the Interfaces for the Services

---
+++ Animal.Domain/Models/Shelter.cs
@@ -0,0 +1,14 @@
+﻿.vs/
+[Bb]in/
+[Oo]bj/
+*.user
+*.suousing System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animal.Domain.Models
+{
+    internal class Shelter
+    {
+    }
+}
